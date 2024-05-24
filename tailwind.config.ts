6c5953d26595dd216ccc7d0a9239f4f4a77defa1
Amaury <iamaury1380@gmail.com>
2024-05-24 04:29:21 +0200

    Update eslint.yml

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
