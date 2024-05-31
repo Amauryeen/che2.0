1b796a6620f80d8068c64229c30b33b5664d47cf
Amaury <iamaury1380@gmail.com>
2024-05-31 19:41:02 +0200

    Optimize PWA

--- app/documents/[id]/download/download.tsx
+++ app/documents/[id]/download/download.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Box, Card, Typography } from '@mui/material';
 import { useEffect } from 'react';
 
 export default function Download(props: { url: string }) {
@@ -7,5 +8,14 @@ export default function Download(props: { url: string }) {
     window.location.href = props.url + '?download=1';
   }, [props.url]);
 
-  return <>Téléchargement du document en cours...</>;
+  return (
+    <Card variant="outlined" sx={{ p: 2 }}>
+      <Box sx={{ display: 'flex' }}>
+        <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+          Téléchargement
+        </Typography>
+      </Box>
+      Téléchargement de votre document en cours...
+    </Card>
+  );
 }
