d306a4e6b80bae5720d638f8d83fe0f48dbaa3bf
Amaury <iamaury1380@gmail.com>
2024-06-22 18:29:15 +0200

    Remove offline page due to security issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
-  dest: "public",
   disable: process.env.NODE_ENV === "development",
-  register: true,
-  reloadOnOnline: true
+  aggressiveFrontEndNavCaching: true,
+  cacheOnFrontEndNav: true
 });
 
 export default withPWA({});
